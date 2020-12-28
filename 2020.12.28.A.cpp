bdac04867cddd7847bb0b36d5d363020b67daf18
y137020 <76675821+y137020@users.noreply.github.com>
2020-12-27 23:18:25 -0600

    Add files via upload

---
+++ 2020.12.28.A.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int A = 0;
+	while (A < 10000)
+	{
+		A += 1;
+		printf("自动敲代码测试,第%d行",A);
+		if (A < 5000)
+			printf(",前半段\n");
+		else
+			printf(",后半段\n");
+	}
+	return 0;
+}
---
+++ 2020.12.28.B.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	int A = 0;
+	printf("背了普地吗？\n");
+	printf("背了打1，没背打0：\n");
+	scanf_s("%d", &A);
+	while (A>=0&&A<=1)
+	{
+		if (A == 1)
+			printf("你普地过了\n");
+		else
+			printf("你必不过\n");
+		return 0;
+	}
+	while (A < 0 || A>1)
+	{
+		printf("说了打1/0，重来\n");
+		scanf_s("%d", &A);
+		while (A >= 0 && A <= 1)
+		{
+			if (A == 1)
+				printf("你普地过了\n");
+			else
+				printf("你必不过\n");
+			return 0;
+		}
+		while (A < 0 || A>1)
+		{
+			printf("傻逼");
+			return 0;
+		}
+	}
+	return 0;
+}
